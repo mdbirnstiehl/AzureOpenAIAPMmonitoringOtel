f07dfa80b0cdcc15e54c969fd1973da9b224e92a
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-06-26 09:26:06 +0200

    Format source code with ruff format

--- counter.py
+++ counter.py
@@ -14,11 +14,13 @@ app = Flask(__name__)
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     api_version=os.getenv("OPENAI_API_VERSION"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
 )
 
 # Monkey-patch the openai.Completion.create function
-client.chat.completions.create = count_completion_requests_and_tokens(client.chat.completions.create)
+client.chat.completions.create = count_completion_requests_and_tokens(
+    client.chat.completions.create
+)
 
 tracer = trace.get_tracer("counter")
 
@@ -29,9 +31,12 @@ def completion():
     response = client.chat.completions.create(
         model="gpt-4",
         messages=[
-            {"role": "user", "content": "How do I send my APM data to Elastic Observability?"}
+            {
+                "role": "user",
+                "content": "How do I send my APM data to Elastic Observability?",
+            }
         ],
         max_tokens=20,
-        temperature=0
+        temperature=0,
     )
-    return(response.choices[0].message.content.strip())
+    return response.choices[0].message.content.strip()
